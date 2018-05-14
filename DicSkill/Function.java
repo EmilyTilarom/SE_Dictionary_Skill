4a781cb850b48c48f0a2b3eb2a3ee40e790feac1
KingEmil <p1grad2@web.de>
2018-05-14 16:51:42 +0200

    Grundgerüst

---
+++ DicSkill/Function.java
@@ -0,0 +1,12 @@
+package DicSkill;
+
+
+/**
+ * 29.04.2018
+ * @author Lia
+ */
+
+public enum Function {
+	
+	TRANSLATION, DEFINITION, SYNONYMS, SPELLING, SCRABBLE_START, SCRABBLE_END, SCRABBLE_CONTAIN;
+}
